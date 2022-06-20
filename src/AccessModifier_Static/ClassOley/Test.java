3c5f5f99914db5d2fe19e35e0dd3d47588e06480
6Thienthien6 <thien502760@gmail.com>
2022-06-20 14:46:33 +0700

    200622

---
+++ src/AccessModifier_Static/ClassOley/Test.java
@@ -0,0 +1,15 @@
+package AccessModifier_Static.ClassOley;
+
+public class Test {
+    private String name;
+    private String classes;
+
+    public static void main(String[] args) {
+
+        OStudent oStudent = new OStudent("Omaske", "Claw");
+
+     oStudent.display();
+
+
+    }
+}
