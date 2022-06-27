fb637950d65e2094d7e5dcd0ce19ce1647062f96
6Thienthien6 <thien502760@gmail.com>
2022-06-27 15:48:41 +0700

    270622

---
+++ src/DSAList/LinkedList/myLinkedListTest.java
@@ -0,0 +1,15 @@
+package DSAList.LinkedList;
+
+public class myLinkedListTest {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        myLinkedList ll = new myLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.printList();
+    }
+}
